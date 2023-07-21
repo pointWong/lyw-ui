d2b8880a3e58de1a1b05267c64fa00368374b4a5
wd <wangdi@loyowo.net>
2023-07-21 11:01:45 +0800

    /lywyui

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/lywui',
   plugins: [
     uni(),
   ],
