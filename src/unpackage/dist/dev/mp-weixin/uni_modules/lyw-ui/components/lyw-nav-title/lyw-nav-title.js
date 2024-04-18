8df36dedac79b07cbf8f8c72465374a67ab0b8ce
wd <wangdi@loyowo.net>
2024-04-18 18:25:29 +0800

    update gitigore

--- src/unpackage/dist/dev/mp-weixin/uni_modules/lyw-ui/components/lyw-nav-title/lyw-nav-title.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const common_vendor = require("../../../../common/vendor.js");
-const _sfc_main = {};
-function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
-  return {};
-}
-const Component = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-209ffd84"], ["__file", "/Users/byt/projects/loyowo/lyw-ui/src/uni_modules/lyw-ui/components/lyw-nav-title/lyw-nav-title.vue"]]);
-wx.createComponent(Component);
