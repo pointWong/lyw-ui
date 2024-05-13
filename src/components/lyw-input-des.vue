a87097452e83caffb66a413e1678f32903b30fbd
pw <505254843@qq.com>
2024-05-13 19:17:49 +0800

    modified:   src/components/lyw-input-des.vue

--- src/components/lyw-input-des.vue
+++ src/components/lyw-input-des.vue
@@ -7,9 +7,18 @@
 <script setup>
 const code = `
   <template>
-    ...
-    <lyw-input></lyw-input>
-    ...
+    <div class="p-3">
+      <div>使用示例</div>
+      <lyw-input class="mt-3" v-model="word"></lyw-input>
+      <div class="mt-4">输入了：</div>
+      <div>{{ word }}</div>
+    </div>
   </template>
+  <script setup>
+    import { ref } from "vue";
+
+    const word = ref('')
+
+  <\/script>
 `
 </script>
